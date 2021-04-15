758ad99f26e925f48db63879b28fc83795e941fc
Krishna Nanda <krishnaisb4u@gmail.com>
2021-04-15 16:35:43 +0530

    Add files via upload

---
+++ application_logging/logger.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+class App_Logger:
+    def __init__(self):
+        pass
+    def log(self,file_Object,log_message):
+        self.now=datetime.now()
+        self.date=self.now.date()
+        self.current_time=self.now.strftime("%H:%M:%S")
+        file_Object.write(
+            str(self.date)+"/"+str(self.current_time)+"\t\t"+log_message+"\n"
+        )
